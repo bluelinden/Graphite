8d4c1a8a7a2619a5c3c0e2744c6c5db87ad4d88e
Timon Schelling <me@timon.zip>
2025-09-21 14:45:00 +0000

    tmp

--- node-graph/wgpu-executor/src/context.rs
+++ node-graph/wgpu-executor/src/context.rs
@@ -52,3 +52,46 @@ impl Context {
 		})
 	}
 }
+
+struct ContextBuilder {
+	backends: wgpu::Backends,
+	features: wgpu::Features,
+}
+impl ContextBuilder {
+	pub fn new() -> Self {
+		Self {
+			backends: wgpu::Backends::all(),
+			features: wgpu::Features::empty(),
+		}
+	}
+	pub fn with_backends(mut self, backends: wgpu::Backends) -> Self {
+		self.backends = backends;
+		self
+	}
+	pub fn with_features(mut self, features: wgpu::Features) -> Self {
+		self.features = features;
+		self
+	}
+	pub fn build(self) -> Context {
+		todo!()
+	}
+	fn build_instance(&self) -> wgpu::Instance {
+		wgpu::Instance::new(&wgpu::InstanceDescriptor {
+			backends: self.backends,
+			..Default::default()
+		})
+	}
+	async fn request_adapter(&self, instance: &wgpu::Instance) -> Option<wgpu::Adapter> {
+		let adapter_options = wgpu::RequestAdapterOptions {
+			power_preference: wgpu::PowerPreference::HighPerformance,
+			compatible_surface: None,
+			force_fallback_adapter: false,
+		};
+		instance.request_adapter(&adapter_options).await.ok()
+	}
+	fn select_adapter(&self, instance: &wgpu::Instance, select: fn(Vec<wgpu::Adapter>) -> Option<wgpu::Adapter>) -> Option<wgpu::Adapter> {
+		select(instance.enumerate_adapters(self.backends))
+	}
+	async fn request_device(&self)
+
+}
